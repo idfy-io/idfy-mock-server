f9177c17f4aa41cc1dff6ff53e848365b0edac26
Christoffer Slettan <christofferslettan@gmail.com>
2018-11-30 13:02:50 +0100

    Update MockGeneratorTest.cs

--- src/Idfy.MockServer.Tests/MockGeneratorTest.cs
+++ src/Idfy.MockServer.Tests/MockGeneratorTest.cs
@@ -52,6 +52,24 @@ namespace Idfy.MockServer.Tests
             Assert.AreEqual(404, mockResponse.StatusCode);
         }
 
+        [Test]
+        public void PrioritizesExactRouteMatch()
+        {
+            // Sometimes, the request path will match multiple paths in our Swagger definition.
+            // For example, the request path "/documents/summary" matches both the "/documents/summary"
+            // and "/documents/{documentId}" paths (we don't have type constraints on path params, so "summary" could technically be
+            // a document ID. Therefore we should exact matches must have priority over paths with params.
+            var request = MockRequest("/signature/documents/summary", "GET");
+            var mockResponse = MockGenerator.Generate(request, _swaggerDocument);
+            
+            Assert.IsNotNull(mockResponse);
+            Assert.AreEqual(200, mockResponse.StatusCode);
+
+            // Ensure that the response contains the list of document summaries
+            var jObj = JObject.FromObject(mockResponse.ResponseBody);
+            Assert.IsNotEmpty(jObj["data"].Value<IEnumerable<object>>());
+        }
+
         public HttpRequest MockRequest(string path, string method)
         {
             var context = new DefaultHttpContext();
